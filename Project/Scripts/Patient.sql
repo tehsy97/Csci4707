d486f2c4c1e950c769344b1b8c52db138d60aa60
Brandon Xu <xuxx1289@umn.edu>
2019-11-18 15:05:12 -0600

    fix patient script

--- Project/Scripts/Patient.sql
+++ Project/Scripts/Patient.sql
@@ -1,7 +1,17 @@
-create table Parent
+create table Patient
 (
-	Parent_id int not null primary key,
+	PId int default 0 not null
+		primary key,
 	FName varchar(30) null,
 	MName varchar(30) null,
-	LName varchar(30) null
+	LName varchar(30) null,
+	House_number int null,
+	Street varchar(100) null,
+	City varchar(30) null,
+	State varchar(30) null,
+	Zip int null,
+	Day int null,
+	Month int null,
+	Year int null,
+	Has_insurance tinyint(1) null
 );
